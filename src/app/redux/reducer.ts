aafd604004ec37567f09c04063e9aed82f49f8da
st-thiennguyen <thien.nguyen@supremetech.vn>
2023-09-05 12:00:54 +0700

    updated convention for filename and position of code

--- src/app/redux/reducer.ts
+++ src/app/redux/reducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import { productReducer } from "./reducer/product-reducer";
-import { cartReducer } from "./reducer/cart-reducer";
+import { productReducer } from "./reducer/product.reducer";
+import { cartReducer } from "./reducer/cart.reducer";
 
 export const rootReducer = combineReducers({
     products: productReducer,
